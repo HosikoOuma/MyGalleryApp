ed55a2f7e7c3bd01cdf6ea18a95efa24d76d1761
HosikoOuma <LoxDosi@yandex.ru>
2025-10-15 21:51:14 +0300

    Final work

--- app/src/main/java/com/example/nkdsify/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nkdsify/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.example.mygalleryapp.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val LightColors = lightColorScheme(
     primary = androidx.compose.ui.graphics.Color(0xFF6750A4),
@@ -20,10 +24,13 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun MyPhotoAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColors
-    } else {
-        LightColors
+    val context = LocalContext.current
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(context)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(context)
+        darkTheme -> DarkColors
+        else -> LightColors
     }
 
     MaterialTheme(
