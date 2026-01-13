e51f376107304063c23139d85a3bd55c959f317f
HosikoOuma <LoxDosi@yandex.ru>
2026-01-14 00:15:44 +0300

    2.0.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nkds.hosikoouma.nekolery"
         minSdk = 30
         targetSdk = 36
-        versionCode = 38
-        versionName = "2.0.18"
+        versionCode = 39
+        versionName = "2.0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
